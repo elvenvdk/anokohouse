d9069d2d978fdc4c1c06e3ee69b065b421272489
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2019-06-12 17:44:26 -0400

    Modified mailList reducer

--- client/src/reducers/mailListReducer.js
+++ client/src/reducers/mailListReducer.js
@@ -1,9 +1,8 @@
-
 const INITIAL_STATE = {};
 
-export default (state = INITIAL_STATE, action) {
-  switch(action.type) {
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
     default:
-        return state;
+      return state;
   }
-}
+};
