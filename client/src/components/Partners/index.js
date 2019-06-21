c8939b3b441418bfa2100a4605e62bb34f621b0f
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2019-06-21 12:23:29 -0400

    Fixed commit issues

---
+++ client/src/components/Partners/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Partners extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Partners;
--- client/src/components/Section/index.js
+++ client/src/components/Section/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 const section = ({ sectionId, sectionClassName, children, title }) => (
   <div className={`Section__${sectionClassName}`} id={sectionId}>
-    {title ? (
-      <h3 className={`Section__${sectionClassName}-Title`}>{title}</h3>
-    ) : (
-      ''
-    )}
+    {title && <h3 className={`Section__${sectionClassName}-Title`}>{title}</h3>}
     {children}
   </div>
 );
